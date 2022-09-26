10123d862464d29130faa8589e43615aed704c3f
Vnbjilu <vikas1.srivastava@gmail.com>
2022-09-26 18:58:23 +0530

    Main Menu Created

--- Phase1FileOperationProject/src/vikas/RatanRaman/Menu.java
+++ Phase1FileOperationProject/src/vikas/RatanRaman/Menu.java
@@ -7,6 +7,14 @@ public class Menu {
 		System.out.println("\n\t\t AppName       : "+appName);
 		System.out.println("\n\t\t DeveloperName : "+devName);
 	}
+	public void mainMenu()
+	{
+		System.out.print("\n\n\n\t======================= Welcome to Main Menu=====================================");
+		System.out.print("\n\n\t 1-> for Display All the Files ");
+		System.out.print("\n\n\t 2->for File Operation ");
+		System.out.print("\n\n\t 3-> for Exit the program ");
+		
+	}
 	
 
 	
