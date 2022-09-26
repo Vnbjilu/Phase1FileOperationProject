5f75e2381f5d9c3a517a58422eff799117fb3571
Vnbjilu <vikas1.srivastava@gmail.com>
2022-09-26 18:55:06 +0530

    Devloper and App Name Display Function Created

--- Phase1FileOperationProject/src/vikas/RatanRaman/Menu.java
+++ Phase1FileOperationProject/src/vikas/RatanRaman/Menu.java
@@ -1,5 +1,13 @@
 package vikas.RatanRaman;
 
 public class Menu {
+	public void details(String appName,String devName)
+	{
+		System.out.println("\n\n\t======================App Details and Developer Name=========================");
+		System.out.println("\n\t\t AppName       : "+appName);
+		System.out.println("\n\t\t DeveloperName : "+devName);
+	}
+	
 
+	
 }
